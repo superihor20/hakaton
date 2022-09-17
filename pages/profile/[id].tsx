d5af4e64c38cfdc5c302c27973ecc464899e399e
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-09-17 17:07:33 +0300

    add profile page

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { UserService } from '../../src/services/UserService';
 import { IUser } from '../../src/models/user';
 import { SERVER_URL } from '../../src/constants/url';
+import { Loader } from '../../src/components/loader/Loader';
+import classes from '/styles/profile-page.module.scss';
 
 const ProfilePage = () => {
   const router = useRouter();
@@ -23,9 +25,17 @@ const ProfilePage = () => {
     }
   }, [id]);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <div>
+    <div className={classes.container}>
       <img src={`${SERVER_URL}/${user.avatar}`} />
+      <span>
+        {user.firstName} {user.lastName}
+      </span>
+      <span>{user.profession}</span>
     </div>
   );
 };
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,17 @@
 export interface IUser {
   id: number;
-  password: string;
   avatar: string;
   firstName: string;
   lastName: string;
   infoId: number;
   login: string;
   profession: string;
+  telegram?: string;
+  instagram?: string;
+  linkedIn?: string;
+}
+
+interface Table {
+  id: number;
+  user: IUser;
 }
