50415144d3c06c6ca0afa1ac853794a4ad4fc998
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-09-17 15:06:59 +0300

    add login form and header

---
+++ models/user.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    id: number
+    email: string
+    password: string
+    avatar: string
+    firstName: string
+    lastName: string
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Head} from "next/document";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Component {...pageProps} />
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+            </Head>
+        </>)
 }
 
 export default MyApp
